afa8a8aabd51acd363b9279ce76a2ea6de812285
Ledio Dylgjeri <lediodylgjeri@gamil.com>
2023-03-22 11:59:03 -0400

    add toggle functionality

--- SingleQuestion.jsx
+++ SingleQuestion.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import { useState } from 'react'
+import {AiOutlineMinus, AiOutlinePlus} from 'react-icons/ai'
 
-const SingleQuestion = () => {
+const SingleQuestion = ({title, info}) => {
+  const [showInfo, setShowInfo] = useState(false)
+
   return (
-    <div>SingleQuestion</div>
-  )
+    <article className="question">
+      <header>
+        <h5>{title}</h5>
+        {showInfo ? (
+          <button
+            className="question-btn"
+            onClick={() => setShowInfo(!showInfo)}
+          >
+            <AiOutlineMinus />
+          </button>
+        ) : (
+          <button
+            className="question-btn"
+            onClick={() => setShowInfo(!showInfo)}
+          >
+            <AiOutlinePlus />
+          </button>
+        )}
+      </header>
+      {showInfo && <p>{info}</p>}
+    </article>
+  );
 }
 
 export default SingleQuestion
